57c4bcd822ced1a8926379e0c83c820c88a3e1c4
Piotr Rzysko <piotr.rzysko@allegro.pl>
2023-08-01 16:14:00 +0200

    OS tools

---
+++ src/jmh/java/pl/allegro/tech/eden/performancefaultyapp/OS.java
@@ -0,0 +1,36 @@
+package pl.allegro.tech.eden.performancefaultyapp;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+public class OS {
+
+    private static final Logger logger = LoggerFactory.getLogger(OS.class);
+
+    public static void clearPageCache() {
+        executeAndWait("sudo sync && echo 3 | sudo tee /proc/sys/vm/drop_caches");
+    }
+
+    public static void executeAndWait(String cmd) {
+        try {
+            execute(cmd).waitFor();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Process execute(String cmd) {
+        logger.info("executing: {}", cmd);
+
+        ProcessBuilder processBuilder = new ProcessBuilder("/bin/bash", "-c", cmd)
+                .inheritIO()
+                .redirectErrorStream(true);
+        try {
+            return processBuilder.start();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
