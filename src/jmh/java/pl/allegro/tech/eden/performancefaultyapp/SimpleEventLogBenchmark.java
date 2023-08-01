b41479727d035895d4cef35cab8df6f96fbba6e5
Daniel Fąderski <faderskd@users.noreply.github.com>
2023-08-01 16:42:45 +0200

    MMap vs Syscall benchmark

--- src/jmh/java/pl/allegro/tech/eden/performancefaultyapp/SimpleEventLogBenchmark.java
+++ src/jmh/java/pl/allegro/tech/eden/performancefaultyapp/SimpleEventLogBenchmark.java
@@ -1,49 +1,69 @@
 package pl.allegro.tech.eden.performancefaultyapp;
 
 import java.io.IOException;
-import java.util.Objects;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Level;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 
 @State(Scope.Benchmark)
-@BenchmarkMode(Mode.AverageTime)
-@OutputTimeUnit(TimeUnit.SECONDS)
+@BenchmarkMode(Mode.Throughput)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
 public class SimpleEventLogBenchmark {
 
-//    @State(Scope.Thread)
-//    public static class State {
-//        public int a = 1;
-//        public int b = 2;
-//        public int sum ;
-//    }
+    public enum EventLogType {
+        Simple {
+            @Override
+            public EventLog create(EventLogProperties properties) throws IOException {
+                return new SimpleEventLog(properties);
+            }
+        },
+        MMap {
+            @Override
+            public EventLog create(EventLogProperties properties) throws IOException {
+                return new MMapEventLog(properties);
+            }
+        };
+        public abstract EventLog create(EventLogProperties properties) throws IOException;
+    }
+
+    @Param
+    EventLogType eventLogType;
+
+    @State(Scope.Thread)
+    public static class EventLogState {
+        public int numberOfStoredEvents = 1000;
+        public String content = "Hello World";
+
+        public int key = ThreadLocalRandom.current().nextInt(1000);
+    }
 
     private final EventLogProperties properties = new EventLogProperties();
-    private SimpleEventLog eventLog;
+    private EventLog eventLog;
 
     @Setup(Level.Trial)
-    public void setUp() throws IOException {
-        int numberOfStoredEvents = 1000;
-        String content = "Hello World";
-        eventLog = new SimpleEventLog(properties);
-        for (int i = 0; i < numberOfStoredEvents; i++) {
-          eventLog.store(new Event(content));
+    public void setUp(EventLogState state) throws IOException {
+        Path path = Path.of(properties.getLogFilePath());
+        if (Files.exists(path)) {
+            Files.delete(path);
+        }
+        eventLog = eventLogType.create(properties);
+        for (int i = 0; i < state.numberOfStoredEvents; i++) {
+            eventLog.store(new Event(state.content));
         }
     }
 
     @Benchmark
-    public void readFromLog() {
-        int numberOfStoredEvents = 1;
-        String expectedContent = "Hello World";
-        for (int i = 0; i < numberOfStoredEvents; i++) {
-            String content = eventLog.get(i).content();
-            assert Objects.equals(content, expectedContent);
-        }
+    public String readFromLog(EventLogState state) {
+        return eventLog.get(state.key++ % state.numberOfStoredEvents).content();
     }
 }
